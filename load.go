8093c26698d6be4ac8520a119ea66b5190a96463
Seebs <seebs@seebs.net>
2018-09-23 12:48:54 -0500

    separate load and store functionality out, clean up nbt load slightly

---
+++ load.go
@@ -0,0 +1,317 @@
+package nbt
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"math"
+	"unsafe"
+)
+// Functions related to loading NBT tags from streams.
+
+// LoadByte loads a Byte payload.
+func LoadByte(r io.Reader) (b Byte, e error) {
+	var buf [1]byte
+	_, err := io.ReadFull(r, buf[0:1])
+	if err != nil {
+		return b, err
+	}
+	return Byte(buf[0]), nil
+}
+
+// LoadShort loads a Short payload.
+func LoadShort(r io.Reader) (s Short, e error) {
+	var buf [2]byte
+	_, err := io.ReadFull(r, buf[0:2])
+	if err != nil {
+		return s, err
+	}
+	return Short(int16(buf[0])<<8 | int16(buf[1])), nil
+}
+
+// LoadInt loads an Int payload.
+func LoadInt(r io.Reader) (i Int, e error) {
+	var buf [4]byte
+	_, err := io.ReadFull(r, buf[0:4])
+	if err != nil {
+		return i, err
+	}
+	return Int(int32(buf[0])<<24 | int32(buf[1])<<16 | int32(buf[2])<<8 | int32(buf[3])), nil
+}
+
+// LoadLong loads a Long payload.
+func LoadLong(r io.Reader) (l Long, e error) {
+	var buf [8]byte
+	_, err := io.ReadFull(r, buf[0:8])
+	if err != nil {
+		return l, err
+	}
+	return Long(
+		int64(buf[0])<<56 |
+			int64(buf[1])<<48 |
+			int64(buf[2])<<40 |
+			int64(buf[3])<<32 |
+			int64(buf[4])<<24 |
+			int64(buf[5])<<16 |
+			int64(buf[6])<<8 |
+			int64(buf[7])<<0), nil
+}
+
+// LoadFloat loads a Float payload.
+func LoadFloat(r io.Reader) (f Float, e error) {
+	var buf [4]byte
+	_, err := io.ReadFull(r, buf[0:4])
+	if err != nil {
+		return f, err
+	}
+	return Float(math.Float32frombits(uint32(buf[0])<<24 | uint32(buf[1])<<16 | uint32(buf[2])<<8 | uint32(buf[3]))), nil
+}
+
+// LoadDouble loads a Double payload.
+func LoadDouble(r io.Reader) (d Double, e error) {
+	var buf [8]byte
+	_, err := io.ReadFull(r, buf[0:8])
+	if err != nil {
+		return d, err
+	}
+	return Double(math.Float64frombits(uint64(buf[0])<<56 |
+		uint64(buf[1])<<48 |
+		uint64(buf[2])<<40 |
+		uint64(buf[3])<<32 |
+		uint64(buf[4])<<24 |
+		uint64(buf[5])<<16 |
+		uint64(buf[6])<<8 |
+		uint64(buf[7])<<0)), nil
+}
+
+// LoadByteArray loads a byte array, which has a leading Int indicating
+// how many bytes it contains.
+func LoadByteArray(r io.Reader) (b ByteArray, e error) {
+	l, err := LoadInt(r)
+	if err != nil {
+		return b, err
+	}
+	buf := make([]byte, int(l))
+	_, err = io.ReadFull(r, buf)
+	if err != nil {
+		return b, err
+	}
+	return *(*[]int8)(unsafe.Pointer(&buf)), err
+}
+
+// LoadIntArray loads an Int array, which has a leading Int indicating
+// how many Ints it contains.
+func LoadIntArray(r io.Reader) (ia IntArray, e error) {
+	l, err := LoadInt(r)
+	if err != nil {
+		return ia, err
+	}
+	buf := make([]Int, int(l))
+	for i := 0; i < int(l); i++ {
+		buf[i], e = LoadInt(r)
+		if e != nil {
+			return ia, e
+		}
+	}
+	return buf, nil
+}
+
+// LoadLongArray loads an Int array, which has a leading Int indicating
+// how many Long it contains.
+func LoadLongArray(r io.Reader) (ia LongArray, e error) {
+	l, err := LoadInt(r)
+	if err != nil {
+		return ia, err
+	}
+	buf := make([]Long, int(l))
+	for i := 0; i < int(l); i++ {
+		buf[i], e = LoadLong(r)
+		if e != nil {
+			return ia, e
+		}
+	}
+	return buf, nil
+}
+
+// LoadString loads a String payload, reading first a Short payload
+// for the string's length, then that many bytes of string data.
+func LoadString(r io.Reader) (s String, e error) {
+	sl, err := LoadShort(r)
+	if err != nil {
+		return s, err
+	}
+	buf := make([]byte, sl)
+	_, err = io.ReadFull(r, buf)
+	if err != nil {
+		return s, err
+	}
+	return String(buf), nil
+}
+
+// LoadList loads a List tag.
+func LoadList(r io.Reader) (l List, e error) {
+	ttype, e := LoadByte(r)
+	if e != nil {
+		return l, e
+	}
+	if Tag(ttype) < TagEnd || Tag(ttype) >= TagMax {
+		return l, fmt.Errorf("invalid tag type for list: %d", ttype)
+	}
+	count, e := LoadInt(r)
+	if e != nil {
+		return l, e
+	}
+	if count < 0 {
+		return l, fmt.Errorf("invalid negative count for list: %d", count)
+	}
+	l.typ = Tag(ttype)
+	l.data = make([]Payload, count)
+	// fmt.Printf("list: %v[%d]\n", l.typ, count)
+	for i := 0; i < int(count); i++ {
+		// fmt.Printf("item %d\n", i)
+		l.data[i], e = LoadPayload(l.typ, r)
+		if e != nil {
+			fmt.Printf("list failed at %d: %s\n", i, e)
+			break
+		}
+	}
+	return l, e
+}
+
+// LoadCompound loads a Compound tag, thus, loads other tags until it gets
+// a TagEnd.
+func LoadCompound(r io.Reader) (c Compound, e error) {
+	c = make(map[string]NBT)
+	var n NBT
+	var err error
+	var errored error // an error we handle after the fact
+	for n, err = LoadUncompressed(r); err == nil && n.Type != TagEnd; n, err = LoadUncompressed(r) {
+		// fmt.Printf("loaded tag: [%v] %s\n", n.Type, n.Name)
+		_, ok := c[n.Name]
+		if ok {
+			// note the thing, but continue using the newer one
+			errored = fmt.Errorf("duplicate name '%s' in compound tag", n.Name)
+		}
+		c[n.Name] = n
+	}
+	if n.Type != TagEnd {
+		fmt.Printf("failed load compound\n")
+		return c, fmt.Errorf("unterminated compound tag")
+	}
+	return c, errored
+}
+
+// LoadPayload wraps the type-specific payload loaders.
+func LoadPayload(typ Tag, r io.Reader) (p Payload, e error) {
+	switch typ {
+	case TagByte:
+		return LoadByte(r)
+	case TagShort:
+		return LoadShort(r)
+	case TagInt:
+		return LoadInt(r)
+	case TagLong:
+		return LoadLong(r)
+	case TagFloat:
+		return LoadFloat(r)
+	case TagDouble:
+		return LoadDouble(r)
+	case TagByteArray:
+		return LoadByteArray(r)
+	case TagString:
+		return LoadString(r)
+	case TagList:
+		return LoadList(r)
+	case TagCompound:
+		return LoadCompound(r)
+	case TagIntArray:
+		return LoadIntArray(r)
+	case TagLongArray:
+		return LoadLongArray(r)
+	}
+	return nil, fmt.Errorf("unhandled type %v", typ)
+}
+
+// Load reads the first NBT found in the gzipped stream r.
+func LoadCompressed(r io.Reader) (NBT, error) {
+	uncomp, err := gzip.NewReader(r)
+	if err != nil {
+		return NBT{}, err
+	}
+	defer uncomp.Close()
+	return LoadUncompressed(uncomp)
+}
+
+// LoadUncompressed reads the first NBT tag found in the uncompressed
+// stream r.
+func LoadUncompressed(r io.Reader) (NBT, error) {
+	var tagByte [1]byte
+	_, err := io.ReadFull(r, tagByte[0:1])
+	if err != nil {
+		return NBT{}, err
+	}
+	tag := Tag(tagByte[0])
+	n := NBT{Type: tag}
+	if n.Type == TagEnd {
+		return n, nil
+	}
+	// every tag other than TagEnd has a name:
+	name, err := LoadString(r)
+	if err != nil {
+		return n, err
+	}
+	n.Name = string(name)
+	// fmt.Printf("load: %s [%v]\n", n.Name, n.Type)
+	switch n.Type {
+	case TagByte:
+		n.payload, err = LoadByte(r)
+	case TagShort:
+		n.payload, err = LoadShort(r)
+	case TagInt:
+		n.payload, err = LoadInt(r)
+	case TagLong:
+		n.payload, err = LoadLong(r)
+	case TagFloat:
+		n.payload, err = LoadFloat(r)
+	case TagDouble:
+		n.payload, err = LoadDouble(r)
+	case TagByteArray:
+		n.payload, err = LoadByteArray(r)
+	case TagString:
+		n.payload, err = LoadString(r)
+	case TagList:
+		n.payload, err = LoadList(r)
+	case TagCompound:
+		n.payload, err = LoadCompound(r)
+	case TagIntArray:
+		n.payload, err = LoadIntArray(r)
+	case TagLongArray:
+		n.payload, err = LoadLongArray(r)
+	default:
+		err = fmt.Errorf("unsupported tag type %v", n.Type)
+	}
+	if err != nil {
+		fmt.Printf("failed to load %s: %s\n", n.Name, err)
+	}
+	return n, err
+}
+
+// Load attempts to determine whether the stream r is compressed or not,
+// and use LoadCompressed/LoadUncompressed accordingly.
+func Load(r io.Reader) (NBT, error) {
+	buf := bufio.NewReader(r)
+	header, err := buf.Peek(512)
+	// couldn't read the thing
+	if err != nil && err != io.EOF {
+		return NBT{}, err
+	}
+	readBuf := bytes.NewBuffer(header)
+	gz, err := gzip.NewReader(readBuf)
+	if err == nil {
+		gz.Close()
+		return LoadCompressed(buf)
+	}
+	return LoadUncompressed(buf)
+}
---
+++ store.go
@@ -0,0 +1,211 @@
+package nbt
+
+import (
+	"fmt"
+	"compress/gzip"
+	"io"
+	"math"
+	"unsafe"
+)
+
+// functionality related to storing NBT data to streams
+
+
+// Store stores `n` to the provided io.Writer. It does
+// not handle compression; for that, use the non-method Store.
+func (n NBT) Store(w io.Writer) error {
+	// TagEnd doesn't get its name written.
+	if n.Type == TagEnd {
+		_, err := w.Write([]byte{0})
+		return err
+	}
+	l := len(n.Name)
+	var b [3]byte
+	b[0] = byte(n.Type)
+	b[1] = byte((l >> 8) & 0xFF)
+	b[2] = byte(l & 0xFF)
+	_, err := w.Write(b[0:3])
+	if err != nil {
+		return err
+	}
+	_, err = w.Write([]byte(n.Name))
+	if err != nil {
+		return err
+	}
+	return n.payload.store(w)
+}
+
+func (p End) store(w io.Writer) error {
+	return nil
+}
+
+func (p Byte) store(w io.Writer) error {
+	b := [1]byte{byte(p)}
+	_, err := w.Write(b[0:1])
+	return err
+}
+
+func (p Short) store(w io.Writer) error {
+	var b [2]byte
+	b[0] = byte((p >> 8) & 0xFF)
+	b[1] = byte(p & 0xFF)
+	_, err := w.Write(b[0:2])
+	return err
+}
+
+func (p Int) store(w io.Writer) error {
+	var b [4]byte
+	b[0] = byte((p >> 24) & 0xFF)
+	b[1] = byte((p >> 16) & 0xFF)
+	b[2] = byte((p >> 8) & 0xFF)
+	b[3] = byte(p & 0xFF)
+	_, err := w.Write(b[0:4])
+	return err
+}
+
+func (p Long) store(w io.Writer) error {
+	var b [8]byte
+	b[0] = byte((p >> 56) & 0xFF)
+	b[1] = byte((p >> 48) & 0xFF)
+	b[2] = byte((p >> 40) & 0xFF)
+	b[3] = byte((p >> 32) & 0xFF)
+	b[4] = byte((p >> 24) & 0xFF)
+	b[5] = byte((p >> 16) & 0xFF)
+	b[6] = byte((p >> 8) & 0xFF)
+	b[7] = byte(p & 0xFF)
+	_, err := w.Write(b[0:8])
+	return err
+}
+
+func (p Float) store(w io.Writer) error {
+	var b [4]byte
+	f := math.Float32bits(float32(p))
+	b[0] = byte((f >> 24) & 0xFF)
+	b[1] = byte((f >> 16) & 0xFF)
+	b[2] = byte((f >> 8) & 0xFF)
+	b[3] = byte(f & 0xFF)
+	_, err := w.Write(b[0:4])
+	return err
+}
+
+func (p Double) store(w io.Writer) error {
+	var b [8]byte
+	f := math.Float64bits(float64(p))
+	b[0] = byte((f >> 56) & 0xFF)
+	b[1] = byte((f >> 48) & 0xFF)
+	b[2] = byte((f >> 40) & 0xFF)
+	b[3] = byte((f >> 32) & 0xFF)
+	b[4] = byte((f >> 24) & 0xFF)
+	b[5] = byte((f >> 16) & 0xFF)
+	b[6] = byte((f >> 8) & 0xFF)
+	b[7] = byte(f & 0xFF)
+	_, err := w.Write(b[0:8])
+	return err
+}
+
+func (p ByteArray) store(w io.Writer) error {
+	l := Int(len(p))
+	err := l.store(w)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(*(*[]byte)(unsafe.Pointer(&p)))
+	return err
+}
+
+func (p String) store(w io.Writer) error {
+	if len(p) > 32767 {
+		return fmt.Errorf("can't store %d-byte string", len(p))
+	}
+	sh := Short(len(p))
+	err := sh.store(w)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write([]byte(p))
+	return err
+}
+
+func (p List) store(w io.Writer) error {
+	err := Byte(p.typ).store(w)
+	if err != nil {
+		return err
+	}
+	l := Int(len(p.data))
+	err = l.store(w)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < int(l); i++ {
+		if p.data[i].Type() != p.typ {
+			return fmt.Errorf("list mismatch, expecting %v, found %v", p.typ, p.data[i].Type())
+		}
+		// store just the payloads, not the whole objects
+		err = p.data[i].store(w)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p Compound) store(w io.Writer) error {
+	for k, v := range p {
+		n := NBT{Name: k, Type: v.Type, payload: v.payload}
+		err := n.Store(w)
+		if err != nil {
+			return err
+		}
+	}
+	end := NBT{Name: "", Type: TagEnd, payload: nil}
+	return end.Store(w)
+}
+
+func (p IntArray) store(w io.Writer) error {
+	l := Int(len(p))
+	err := l.store(w)
+	if err != nil {
+		return err
+	}
+	for _, i := range p {
+		err = i.store(w)
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (p LongArray) store(w io.Writer) error {
+	l := Int(len(p))
+	err := l.store(w)
+	if err != nil {
+		return err
+	}
+	for _, i := range p {
+		err = i.store(w)
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+// StoreCompressed writes n to w, compressed via gzip.
+func StoreCompressed(w io.Writer, n NBT) error {
+	comp := gzip.NewWriter(w)
+	defer comp.Close()
+	return StoreUncompressed(comp, n)
+}
+
+// StoreUncompressed writes n to w, not compressing it. This is not
+// usually useful except for debugging.
+func StoreUncompressed(w io.Writer, n NBT) error {
+	return n.Store(w)
+}
+
+// Store is just an alias for StoreCompressed, since the NBT spec
+// says everything is gzipped.
+func Store(w io.Writer, n NBT) error {
+	return StoreCompressed(w, n)
+}
